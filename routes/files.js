398b06fe200b29be26d21ba96a3ec1a1cd25964d
Sharad Mehrotra <47598208+sharad2307@users.noreply.github.com>
2021-07-15 23:57:19 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -26,7 +26,7 @@ router.post('/', (req, res) => {
             size: req.file.size
         });
         const response = await file.save();
-        res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` });
+        res.json({ file: `${process.env.APP_BASE_URL}files/${response.uuid}` });
       });
 });
 
@@ -53,7 +53,7 @@ router.post('/send', async (req, res) => {
       text: `${emailFrom} shared a file with you.`,
       html: require('../services/emailTemplate')({
                 emailFrom, 
-                downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email` ,
+                downloadLink: `${process.env.APP_BASE_URL}files/${file.uuid}?source=email` ,
                 size: parseInt(file.size/1000) + ' KB',
                 expires: '24 hours'
             })
